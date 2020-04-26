feb0b8d7472f682347a9e725d3c472c2ebc28969
Percy04 <44750476+Percy04@users.noreply.github.com>
2020-04-26 16:01:09 +0530

    Update Instagrambot.py

--- Instagrambot.py
+++ Instagrambot.py
@@ -66,93 +66,94 @@ class Instabot:
                     'followforfollow', 'follow4follow', 'cute', 'fun',
                     'photography', 'nofilter', 'photoshop', 'alumni',
                     'happy', 'me', 'followme', 'follow', 'instagood']
-
+        
         tag = random.choice(hashtags)
-
-        driver.get("https://www.instagram.com/explore/tags/" + tag + "/")
-        sleep(2)
-
-        # gathering photos
-        pic_hrefs = []
-
-        for i in range(1, 7):
-            try:
-                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-                sleep(2)
-                # get tags
-                hrefs_in_view = driver.find_elements_by_tag_name('a')
-                # finding relevant hrefs
-                hrefs_in_view = [elem.get_attribute('href') for elem in hrefs_in_view
-                                 if '.com/p/' in elem.get_attribute('href')]
-                # building list of unique photos
-                [pic_hrefs.append(href) for href in hrefs_in_view if href not in pic_hrefs]
-                print("Check: pic href length " + str(len(pic_hrefs)))
-            except Exception:
-                pass
-
-        # Liking, following and comment users
-        unique_photos = len(pic_hrefs)
-        followers_list = []  # Contains the names of the people you followed
-
-        for pic_href in pic_hrefs:
-            driver.get(pic_href)
+        
+        while True:
+            driver.get("https://www.instagram.com/explore/tags/" + tag + "/")
             sleep(2)
-            try:
-                # Like this picture
-                driver.find_element_by_xpath("//*[@aria-label='Like']").click()
 
-                follow_button = driver.find_element_by_class_name('bY2yH')
-                # Follow the user if not followed already
-                if follow_button.text == "•\n" + "Follow":
-                    follow_button.click()
-                    followed = driver.find_element_by_class_name('e1e1d')
-                    followers_list.append(followed.text)
-                    with open("file.txt", 'a') as append:  # Add the username to file.txt
-                        append.write(",".join(followers_list))
-                        append.write(",")
+            # gathering photos
+            pic_hrefs = []
 
-                    # comment
-                    comment_prob = random.randint(1, 10)
+            for i in range(1, 7):
+                try:
+                    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                    sleep(2)
+                    # get tags
+                    hrefs_in_view = driver.find_elements_by_tag_name('a')
+                    # finding relevant hrefs
+                    hrefs_in_view = [elem.get_attribute('href') for elem in hrefs_in_view
+                                     if '.com/p/' in elem.get_attribute('href')]
+                    # building list of unique photos
+                    [pic_hrefs.append(href) for href in hrefs_in_view if href not in pic_hrefs]
+                    print("Check: pic href length " + str(len(pic_hrefs)))
+                except Exception:
+                    pass
 
-                    # This makes sure that I comment only a certain percentage of times.
-                    if comment_prob >= 6:
-                        # Clicking the comment box
-                        driver.find_element_by_class_name('Ypffh').click()
+            # Liking, following and comment users
+            unique_photos = len(pic_hrefs)
+            followers_list = []  # Contains the names of the people you followed
 
-                        comment_box_area = driver.find_element_by_xpath(
-                            "/html/body/div[1]/section/main/div/div[1]/article/div[2]/section[3]/div/form/textarea")
-                        # Adding the comment
-                        list_of_comments = ["This is great", "Dope", "This is cool dude",
-                                            "Promotions in the comment sections are so annoying."
-                                            "NO, I am not going to tell you to follow me",
-                                            "ayy nice one",
-                                            "How do I get more followers? Make a bot they said",
-                                            "This is what you came for - Rihanna",
-                                            "Roast me",
-                                            "Tell me a few things that you are doing during this quarantine",
-                                            "Damn dude. Who else is hella bored ?",
-                                            "I am super bored. So comment whether you wanna join or not and follow me"
-                                            "and I will add you to a quarantine group chat. "
-                                            "Where we all just chat and chill"]
-
-                        comment_box_area.send_keys(random.choice(list_of_comments))
-
-                        # Click Enter to post comment
-                        comment_box_area.send_keys(Keys.ENTER)
-                        sleep(3)
-                    else:
-                        pass
-
-                else:
-                    continue
-
-                for second in reversed(range(0, 3)):
-                    print_same_line("#" + tag + ': unique photos left: ' + str(unique_photos)
-                                    + " | Sleeping " + str(second))
-                    sleep(1)
-            except Exception:
+            for pic_href in pic_hrefs:
+                driver.get(pic_href)
                 sleep(2)
-            unique_photos -= 1
+                try:
+                    # Like this picture
+                    driver.find_element_by_xpath("//*[@aria-label='Like']").click()
+
+                    follow_button = driver.find_element_by_class_name('bY2yH')
+                    # Follow the user if not followed already
+                    if follow_button.text == "•\n" + "Follow":
+                        follow_button.click()
+                        followed = driver.find_element_by_class_name('e1e1d')
+                        followers_list.append(followed.text)
+                        with open("file.txt", 'a') as append:  # Add the username to file.txt
+                            append.write(",".join(followers_list))
+                            append.write(",")
+
+                        # comment
+                        comment_prob = random.randint(1, 10)
+
+                        # This makes sure that I comment only a certain percentage of times.
+                        if comment_prob >= 6:
+                            # Clicking the comment box
+                            driver.find_element_by_class_name('Ypffh').click()
+
+                            comment_box_area = driver.find_element_by_xpath(
+                                "/html/body/div[1]/section/main/div/div[1]/article/div[2]/section[3]/div/form/textarea")
+                            # Adding the comment
+                            list_of_comments = ["This is great", "Dope", "This is cool dude",
+                                                "Promotions in the comment sections are so annoying."
+                                                "NO, I am not going to tell you to follow me",
+                                                "ayy nice one",
+                                                "How do I get more followers? Make a bot they said",
+                                                "This is what you came for - Rihanna",
+                                                "Roast me",
+                                                "Tell me a few things that you are doing during this quarantine",
+                                                "Damn dude. Who else is hella bored ?",
+                                                "I am super bored. So comment whether you wanna join or not and follow me"
+                                                "and I will add you to a quarantine group chat. "
+                                                "Where we all just chat and chill"]
+
+                            comment_box_area.send_keys(random.choice(list_of_comments))
+
+                            # Click Enter to post comment
+                            comment_box_area.send_keys(Keys.ENTER)
+                            sleep(3)
+                        else:
+                            pass
+
+                    else:
+                        continue
+
+                    for second in reversed(range(0, 3)):
+                        print_same_line("#" + tag + ': unique photos left: ' + str(unique_photos)
+                                        + " | Sleeping " + str(second))
+                        sleep(1)
+                except Exception:
+                    sleep(2)
+                unique_photos -= 1
 
     def unfollow(self):
         """
